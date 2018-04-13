12b0d31891da2e65dbfb1a9d5f50b97b530eb8da
dcravens <35239728+dcravens@users.noreply.github.com>
2018-04-12 23:59:43 -0400

    updated json

--- assignments/final/scripts/servicetable.js
+++ assignments/final/scripts/servicetable.js
@@ -1,19 +1,22 @@
 //pricing data fro service table on services page
-var requestURL = '//dcravens.github.io/assignments/final/data/servicedata.json';
+var requestURL = 'data/servicedata.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL, true);
 request.send();
 request.onload = function() {
-var tablePrices = JSON.parse(request.responseText);
-var safetyCheck = tablePrices.safetyCheck;
-var tuneUp = tablePrices.proTuneUp;
-var tuneClean = tablePrices.proTuneUpClean;
-var wheelService = tablePrices.wheelService;
-var suspenService = tablePrices.suspensionService;
-document.getElementById("safety").innerHTML = safetyCheck;
-document.getElementById("protune").innerHTML = tuneUp;
-document.getElementById("ptclean").innerHTML = tuneClean;
-document.getElementById("wheel").innerHTML = wheelService;
-document.getElementById("suspension").innerHTML = suspenService;
 
+    alert(request.responseText);
+
+    var tablePrices = JSON.parse(request.responseText);
+    var safetyCheck = tablePrices.prices[0];
+    var tuneUp = tablePrices.prices.proTuneUp;
+    var tuneClean = tablePrices.prices.proTuneUpClean;
+    var wheelService = tablePrices.prices.wheelService;
+    var suspenService = tablePrices.prices.suspensionService;
+
+    document.getElementById("safety").innerHTML = safetyCheck;
+    document.getElementById("protune").innerHTML = tuneUp;
+    document.getElementById("ptclean").innerHTML = tuneClean;
+    document.getElementById("wheel").innerHTML = wheelService;
+    document.getElementById("suspension").innerHTML = suspenService;
 }
