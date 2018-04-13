0048d8de91869e97589b10cc8281ab231e7dc322
dcravens <35239728+dcravens@users.noreply.github.com>
2018-04-12 23:19:58 -0400

    json doc correction, js file

--- assignments/final/scripts/servicetable.js
+++ assignments/final/scripts/servicetable.js
@@ -1,6 +1,19 @@
 //pricing data fro service table on services page
-var safetyCheck
-var tuneUp
-var tuneClean
-var wheelService
-var suspenService
+var requestURL = '//dcravens.github.io/assignments/final/data/servicedata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL, true);
+request.send();
+request.onload = function() {
+var tablePrices = JSON.parse(request.responseText);
+var safetyCheck = tablePrices.safetyCheck;
+var tuneUp = tablePrices.proTuneUp;
+var tuneClean = tablePrices.proTuneUpClean;
+var wheelService = tablePrices.wheelService;
+var suspenService = tablePrices.suspensionService;
+document.getElementById("safety").innerHTML = safetyCheck;
+document.getElementById("protune").innerHTML = tuneUp;
+document.getElementById("ptclean").innerHTML = tuneClean;
+document.getElementById("wheel").innerHTML = wheelService;
+document.getElementById("suspension").innerHTML = suspenService;
+
+}
